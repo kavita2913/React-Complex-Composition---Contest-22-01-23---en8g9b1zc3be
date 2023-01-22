2d1ab429651558dfb23ffa5987efaf4ef633187c
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-22 19:54:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ const Fruits = () => {
   return (
     <div id="fruits">
       { /* Change code below this line */ }
-
+  {TypesOfFruit}
       { /* Change code above this line */ }
     </div>
   );
@@ -32,7 +32,7 @@ const App = () => {
     <div id="main">
       <h1>Types of Food:</h1>
       { /* Change code below this line */ }
-
+{ Fruits}
       { /* Change code above this line */ }
     </div>
   )
